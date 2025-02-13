2cb0dc1cf39e666118af2f0d57c38009e058fab2
Ernest Yeboah <yeboahernest246@gmail.com>
2025-02-12 19:38:05 -1200

    word renovated

--- src/context/WordGuessContext.tsx
+++ src/context/WordGuessContext.tsx
@@ -1,28 +1,22 @@
-import { createContext, useContext, useState } from "react";
-import { wordGuess } from "../dataExports/wordGuessData";
+// import { createContext, useContext } from "react";
 
-const WordGuessContext = createContext<Exports | null>(null);
+// const WordGuessContext = createContext<Exports | null>(null);
 
-type ChildrenProps = {
-  children: React.ReactNode;
-};
+// type ChildrenProps = {
+//   children: React.ReactNode;
+// };
 
-type Exports = {
-  currentIndex: number;
-  renderRandomWord(): void;
-};
+// type Exports = {};
 
-// eslint-disable-next-line react-refresh/only-export-components
-export function useGuessWordContext() {
-  const object = useContext(WordGuessContext);
-  if (!object) throw new Error("Please provide a provider");
-  return object;
-}
+// // eslint-disable-next-line react-refresh/only-export-components
+// export function useGuessWordContext() {
+//   const object = useContext(WordGuessContext);
+//   if (!object) throw new Error("Please provide a provider");
+//   return object;
+// }
 
-export default function WordGuessGlobalState({ children }: ChildrenProps) {
-  return (
-    <WordGuessContext.Provider value={{ currentIndex, renderRandomWord }}>
-      {children}
-    </WordGuessContext.Provider>
-  );
-}
+// export default function WordGuessGlobalState({ children }: ChildrenProps) {
+//   return (
+//     <WordGuessContext.Provider value={{}}>{children}</WordGuessContext.Provider>
+//   );
+// }
